665e3de23898d8530a784e1aff697cae3dddaf2f
Moody Rahman <moody.byte@gmail.com>
2023-04-01 23:55:22 -0400

    simOS header skeleton

--- SimOS.h
+++ SimOS.h
@@ -1,1 +1,31 @@
 // Moududur Rahman
+#pragma once
+#include <vector>
+#include <iostream>
+#include <queue>
+class SimOS
+{
+    SimOS(int numberOfDisks, int amountOfRAM);
+    bool NewProcess(int priority, int size);
+    bool SimFork();
+    void SimExit();
+    void SimWait();
+    void DiskReadRequest(int diskNumber, std::string fileName);
+    void DiskJobCompleted(int diskNumber);
+    int GetCPU();
+    std::vector<int> GetReadyQueue();
+    FileReadRequest GetDisk(int diskNumber);
+    std::queue<FileReadRequest> GetDiskQueue(int diskNumber);
+};
+
+struct Process
+{
+    int priority;
+    int size;
+};
+
+struct FileReadRequest
+{
+    int PID;
+    std::string fileName;
+};

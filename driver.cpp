4f154a0a9306b5a73153c447e238acead314c144
Moody Rahman <moody.byte@gmail.com>
2023-05-12 17:07:57 -0400

    driver display

--- driver.cpp
+++ driver.cpp
@@ -4,44 +4,45 @@ int main(int argc, char const *argv[])
 {
 
     SimOS s(5, 500);
-    // s.NewProcess(3, 5);
-    // s.NewProcess(4, 5);
-    // s.NewProcess(4, 5);
-    // s.NewProcess(4, 5);
-    // s.NewProcess(3, 5);
-    // s.NewProcess(3, 5);
-    // s.NewProcess(2, 5);
-    // s.NewProcess(2, 5);
-    // s.NewProcess(5, 5);
-    // s.NewProcess(5, 5);
-    // s.NewProcess(2, 5);
     s.NewProcess(2, 15);
     s.NewProcess(2, 10);
-    s.NewProcess(4, 10);
-    s.NewProcess(3, 5);
-    s.NewProcess(2, 10);
-    s.NewProcess(2, 10);
-    s.NewProcess(3, 5);
-    s.NewProcess(3, 5);
-    s.NewProcess(4, 5);
-    s.NewProcess(3, 5);
 
-    s.SimExit();
-    s.SimExit();
+    s.SimFork();
+    s.SimFork();
 
-    s.NewProcess(2, 600);
-    s.NewProcess(2, 5);
-    s.NewProcess(4, 5);
-    s.NewProcess(4, 5);
-    s.NewProcess(2, 5);
-    s.NewProcess(2, 5);
+    s.NewProcess(3, 5);
+    s.SimExit();
+    s.SimWait();
+    s.SimExit();
+    s.display();
+    s.SimFork();
+    s.display();
+    s.SimWait();
+    s.SimWait();
+    s.SimWait();
+    s.display();
+    s.SimExit();
+    s.display();
+
+    // s.SimFork();
+    // s.SimFork();
+
+    // s.SimWait();
+
+    // s.NewProcess(4, 10);
+
+    // s.NewProcess(2, 10);
+    // s.NewProcess(2, 10);
+    // s.SimFork();
+    // s.SimWait();
+    // s.SimExit();
 
     // std::cout << s.process_queue.top() << std::endl;
-    while (!s.process_queue.empty())
-    {
-        std::cout << s.process_queue.top() << "\n";
-        s.process_queue.pop();
-    }
+    // while (!s.process_queue.empty())
+    // {
+    //     std::cout << s.process_queue[0] << "\n";
+    //     s.process_queue.;
+    // }
 
     // std::cout
     //     << "=============" << std::endl;
